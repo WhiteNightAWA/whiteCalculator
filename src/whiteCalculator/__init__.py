e22b70d3f8ec2c14ef4d87e4f1cdb161f9d2fab7
White_Night_awa <s121101.evantang.2015@gmail.com>
2021-09-16 23:55:25 +0800

    Update files and website

--- src/whiteCalculator/__init__.py
+++ src/whiteCalculator/__init__.py
@@ -9,8 +9,8 @@ This is a proof of concept of a whiteCalculator library, written in Python.
 """
 
 __title__ = "whiteCalculator"
-__version__ = "0.0.6"
-__build__ = 0x000006
+__version__ = "0.0.7"
+__build__ = 0x000007
 __author__ = "WhiteNight"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021 WhiteNight"
